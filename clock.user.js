37ce079a43f0001226d9b483273b8bb4fb43abe2
SenorPyro <41166922+SenorPyro@users.noreply.github.com>
2020-04-28 19:15:05 +0200

    Update clock.user.js

--- clock.user.js
+++ clock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name				MaM Clock changer
 // @namespace			https://nowhere.com
-// @version				1.01
+// @version				1.02
 // @description		Changes site clock from UTC to Nepal time
 // @author				pyrokiller
 // @match				https://www.myanonamouse.net/*
@@ -13,7 +13,7 @@ window.addEventListener('load', embedFunction);
 
 function dT() {
   var a = new Date();
-  a.setSeconds((new Date).getSeconds() - timeOffset + 4 * 3600);
+  a.setSeconds((new Date).getSeconds() - timeOffset + 5.75 * 3600);
   $.each($('.tP'), function (b, c) {
     if ($(this).attr('data-dtype') == '1') {
       $(this).html(a.toUTCString().replace('GMT', ''))
